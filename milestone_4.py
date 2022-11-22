cfe4a09ca64a003bf13ad86cc24cac7c29a45706
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-22 17:37:35 +0000

    removes while loop

--- milestone_4.py
+++ milestone_4.py
@@ -19,19 +19,17 @@ class Hangman:
 
     def check_guess(self, guess):
         guess = guess.lower()
-        while True:
-            if guess in self.word.lower():
-                for i in range(len(self.word)):
-                    if guess == self.word[i]:
-                        self.word_guessed[i] = str(guess)
-                self.num_letters -= 1
-                print("Good guess! " + guess + " is in the word.")
-                break
-            elif guess not in self.word.lower():
-                print("Sorry, " + guess + "is not in the word.")
-                self.num_lives -= 1
-                print("You have " + str(self.num_lives) + " left")
-            self.list_of_guesses.append(guess)
+        if guess in self.word.lower():
+            for i in range(len(self.word)):
+                if guess == self.word[i]:
+                    self.word_guessed[i] = str(guess)
+            self.num_letters -= 1
+            print("Good guess! " + guess + " is in the word.")
+        elif guess not in self.word.lower():
+            print("Sorry, " + guess + "is not in the word.")
+            self.num_lives -= 1
+            print("You have " + str(self.num_lives) + " left")
+        self.list_of_guesses.append(guess)
 
     def ask_for_input(self):
         while True:
