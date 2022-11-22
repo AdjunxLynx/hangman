9c6aafbdbbf08f114b851a213a357a41ecf1790b
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-22 14:22:41 +0000

    adds function to check if letter is in the word

--- milestone_4.py
+++ milestone_4.py
@@ -13,4 +13,32 @@ class Hangman:
 
 
         print(self.word_guessed)
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        correct = False
+        while not correct:
+            if guess in self.word:
+                print("Good guess! '" + guess + "' is in the word.")
+                correct = True
+            else:
+                print("Sorry, " + guess + "is not in the word. Try again.")
+
+    def ask_for_input(self):
+        valid = False
+        guess = input("Guess a letter. ")
+
+        while not valid:
+            if len(guess) == 1:
+                if guess.isalpha():
+                    print("Good guess")
+                    valid = True
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+
+        self.check_guess(guess)
+
+
+
 h = Hangman(["asjs", "sdfhsdvuads"])
+h.check_guess("a")
