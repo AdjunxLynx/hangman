f0dc09894bb534cd70e560a0fad4e7c6bf667d19
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-22 17:59:21 +0000

    introduces while loop

--- milestone_5.py
+++ milestone_5.py
@@ -39,14 +39,17 @@ class Hangman:
 
 
 def play_game(word_list):
+    while True:
+        game = Hangman(word_list, num_lives = 6)
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        elif game.num_lives != 0 and game.num_letters <= 0:
+            print("Congratulations. You won the game!")
+            break
 
-    game = Hangman(word_list, num_lives = 6)
-    if game.num_lives == 0:
-        print("You lost!")
-    elif game.num_letters > 0:
-        game.ask_for_input()
-    elif game.num_lives != 0 and game.num_letters <= 0:
-        print("Congratulations. You won the game!")
 
 
 word_list = ["banana", "apple", "strawberry", "pineapple", "plum"]
