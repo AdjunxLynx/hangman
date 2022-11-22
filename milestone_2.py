3f092d038fbbebcfeca4a230eaef758216ca3665
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-22 00:53:29 +0000

    Adds error detection for guess input

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,10 @@ word_list = ["banana", "apple", "strawberry", "pineapple", "plum"]
 print(word_list)
 guess = input("Guess a letter")
 print(guess)
+
+if len(guess) == 1:
+    if guess.isalpha():
+        print("Good guess")
+
+else:
+    print("Oops! That is not a valid input.")
